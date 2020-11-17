0c490def88a9d5a74f16d8a79c5852d186a3b9f8
tbelhomm <tbelhomm@42.fr>
2020-11-17 12:59:14 +0100

    Add alphanum functions

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: tbelhomm <tbelhomm@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 14:57:33 by tbelhomm          #+#    #+#             */
-/*   Updated: 2020/11/17 12:04:27 by tbelhomm         ###   ########.fr       */
+/*   Updated: 2020/11/17 12:58:47 by tbelhomm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,4 +23,10 @@ void    *ft_memchr(const void *s, int c, size_t n);
 int     ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t  ft_strlen(const char *s);
 int     ft_isalpha(int c);
+int     ft_isdigit(int c);
+int     ft_isalnum(int c);
+int     ft_isascii(int c);
+int     ft_isprint(int c);
+int     ft_toupper(int c);
+int	ft_tolower(int c);
 #endif
